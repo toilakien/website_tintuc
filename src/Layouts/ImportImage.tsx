25a7f5b3849e761e2aa30ef666b17a02b72859b4
Kiên <86771355+toilakien@users.noreply.github.com>
2022-12-29 23:40:25 +0700

    fjf

--- src/Layouts/ImportImage.tsx
+++ src/Layouts/ImportImage.tsx
@@ -5,6 +5,8 @@ import anh5 from "../assets/images/Tin-tuc-24h-phan-sao-nam-2-1631696965-632-wid
 import anh4 from "../assets/images/Tin-tuc-24h-viet-nam-1623847557-236-width645height429.jpg";
 import anh6 from "../assets/images/1586005915-501-tin-tuc-24h-quaxuat-hien-truonghop-f4-dau-tien-o-viet-nam-mac-covid-19-n3-1586005661-width660height398.jpg";
 import anh7 from "../assets/images/1661855180-799-vu-no-o-bac-ninh-da-so-cac-nan-nhan-bi-thuong-do-dam-dap-nhau-luc-thao-chayvu-no-o-bac-ninh-davu-no--hien-truong-2-1661849437-458-width645height352-1661855180-width645height352.jpg";
+import anh8 from "../assets/images/Untitled-2-01-c5f47867.png";
+import anh9 from "../assets/images/it.jpg";
 const image = [
   { title: "Vừa làm nên lịch sử, Mbappe phản ứng bất ngờ với FIFA", anh: anh1 },
   { title: "Tin nóng 24h , xoay quanh câu chuyện giám đốc công an", anh: anh2 },
@@ -17,5 +19,13 @@ const image = [
     anh: anh6,
   },
   { title: "Vừa làm nên lịch sử, Mbappe phản ứng bất ngờ với FIFA", anh: anh7 },
+  {
+    title: "Top 10 Website Tin Tức Công Nghệ Bằng Tiếng Anh Mọi Người Nên Biết",
+    anh: anh8,
+  },
+  {
+    title: "Top 7 trường đại học công nghệ thông tin tốt nhất tại Việt Nam",
+    anh: anh9,
+  },
 ];
 export default image;
